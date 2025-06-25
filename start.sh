4b0ff8b7a4807e64091f703921b6070673f7ce9f
Victorleo02 <leozinhooo.gaspar@gmail.com>
2025-06-25 17:39:36 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod +x /app/Arena\ Nova\ Vida.x86_64
-/app/Arena\ Nova\ Vida.x86_64 --server --port 7777
+chmod +x /app/Arena_Nova_Vida.x86_64
+/app/Arena_Nova_Vida.x86_64 --server --port 7777
